4c01263f4ad30dae589d82445ffc5b8a4685e741
Elton Stoneman <elton@sixeyed.com>
2020-03-13 22:05:22 +0000

    Add whoami image for lab & update build

---
+++ ch02/docker-images/whoami/src/Controllers/WebController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace whoami.Controllers
+{
+    [Produces("application/json")]
+    [Route("/")]
+    public class WebController : Controller
+    {
+        private static string _Host = Dns.GetHostName();
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {             
+            var osDescription = RuntimeInformation.OSDescription;
+            return $"I'm {_Host} running on {osDescription}";
+        }
+    }
+}
---
+++ ch02/lab/README.md
@@ -0,0 +1,11 @@
+kubectl apply -f .\solution\deployment.yaml
+
+kubectl port-forward deploy/whoami 8080:80
+
+curl http://localhost:8080
+
+> "I'm whoami-687976f48b-tkxp9 running on Linux 4.19.76-linuxkit #1 SMP Thu Oct 17 19:31:58 UTC 2019"
+
+kubectl exec deploy/whoami -- sh -c 'hostname'
+
+> whoami-687976f48b-tkxp9
