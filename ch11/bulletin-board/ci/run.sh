8ef7d42914b4f437d45ea86cd1cca2c3b88a647d
Elton Stoneman <elton@sixeyed.com>
2020-06-19 15:13:10 +0100

    Template username in image repo

--- ch11/bulletin-board/ci/run.sh
+++ ch11/bulletin-board/ci/run.sh
@@ -3,7 +3,7 @@
 helm upgrade --install \
   --create-namespace \
   --atomic \
-  --set registryServer=${REGISTRY_SERVER},imageBuildNumber=${BUILD_NUMBER} \
-  --namespace kiamol-test \
+  --set registryServer=${REGISTRY_SERVER},registryUser=${REGISTRY_USER},imageBuildNumber=${BUILD_NUMBER} \
+  --namespace kiamol-ch11 \
   bulletin-board \
   helm/bulletin-board
