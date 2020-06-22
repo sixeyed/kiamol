8b6968e328f215d3be47109aa625c6a02728747d
Elton Stoneman <elton@sixeyed.com>
2020-06-22 15:54:14 +0100

    Jenkins and Gogs updates for ch11

--- ch11/docker-images/jenkins/start.sh
+++ ch11/docker-images/jenkins/start.sh
@@ -7,8 +7,14 @@ kubectl config set-credentials user --token=$(cat /var/run/secrets/kubernetes.io
 kubectl config set-context default --user=user
 kubectl config use-context default
 
-# promote registry user to env:
+# promote registry details to env:
+registry=$(cat ~/.docker/config.json | jq '.auths' | jq 'keys[0]' -r)
+if [ "$registry" = "https://index.docker.io/v1/" ]; then export REGISTRY_SERVER='docker.io'; else export REGISTRY_SERVER=$registry; fi
 export REGISTRY_USER=$(cat ~/.docker/config.json | jq '.auths[].username' -r)
+echo "*** Using registry: $REGISTRY_SERVER, with user: $REGISTRY_USER ***"
 
 # run Jenkins
 java -Duser.home=${JENKINS_HOME} -Djenkins.install.runSetupWizard=false -jar /jenkins/jenkins.war
+
+
+https://index.docker.io/v1/
