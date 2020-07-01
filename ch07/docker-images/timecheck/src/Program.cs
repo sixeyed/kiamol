0b1e2d400fc5b648797496b16bba971c15e571b3
Elton Stoneman <elton@sixeyed.com>
2020-07-01 22:05:27 +0100

    Ch14 - 14.1 & 14.2 exercises

--- ch07/docker-images/timecheck/src/Program.cs
+++ ch07/docker-images/timecheck/src/Program.cs
@@ -2,30 +2,41 @@
 using System.Threading;
 using System.Timers;
 using Microsoft.Extensions.Configuration;
+using Prometheus;
 using Serilog;
 
 namespace Kiamol.Ch07.TimeCheck
 {
     class Program
     {
-        private static ManualResetEvent _ResetEvent = new ManualResetEvent(false);
+        private static readonly ManualResetEvent _ResetEvent = new ManualResetEvent(false);
+        private static  Counter _CheckCounter;
         private static string _Version;
         private static string _Env;
 
         public static void Main()
         {
             var config = new ConfigurationBuilder()
-                             .AddJsonFile("/config/appsettings.json")
+                             .AddJsonFile("appsettings.json")
+                             .AddJsonFile("/config/appsettings.json", optional: true)
+                             .AddEnvironmentVariables()
                              .Build();
 
             _Version = config["Application:Version"];
             _Env = config["Application:Environment"];
-            var intervalSeconds = int.Parse(config["Timer:IntervalSeconds"]) * 1000;
+            var intervalSeconds = config.GetValue<int>("Timer:IntervalSeconds") * 1000;
+            var metricsEnabled = config.GetValue<bool>("Metrics:Enabled");
 
             Log.Logger = new LoggerConfiguration()
                                 .MinimumLevel.Information()
                                 .WriteTo.File("/logs/timecheck.log", shared: true, flushToDiskInterval: TimeSpan.FromSeconds(intervalSeconds))
                                 .CreateLogger();
+
+            if (metricsEnabled)
+            {
+                _CheckCounter = Metrics.CreateCounter("timecheck_total", "Number of timechecks");
+                StartMetricServer(config);
+            }
 
             using (var timer = new System.Timers.Timer(intervalSeconds))
             {
@@ -39,6 +50,19 @@ namespace Kiamol.Ch07.TimeCheck
         {
             Log.Information("Environment: {environment}; version: {version}; time check: {timestamp}",
                                 _Env, _Version, e.SignalTime.ToString("HH:mm.ss"));
+            
+            if (_CheckCounter != null)
+            {
+                _CheckCounter.Inc();
+            }
+        }
+
+        private static void StartMetricServer(IConfiguration config)
+        {
+            var metricsPort = config.GetValue<int>("Metrics:Port");
+            var server = new MetricServer(metricsPort);
+            server.Start();
+            Log.Information("Metrics server listening on port: {metricsPort}", metricsPort);
         }
     }
 }
---
+++ ch14/docker-images/image-gallery/go.mod
@@ -0,0 +1,5 @@
+module kiamol/image-gallery
+
+go 1.14
+
+require github.com/prometheus/client_golang v1.7.1
