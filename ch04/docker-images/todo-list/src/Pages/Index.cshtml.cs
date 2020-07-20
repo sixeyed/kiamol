f24219e3dfc3d828d108d8b81928ce0bbb7f2635
Elton Stoneman <elton@sixeyed.com>
2020-07-20 22:27:25 +0100

    Ch19 exercises

--- ch04/docker-images/todo-list/src/Pages/Index.cshtml.cs
+++ ch04/docker-images/todo-list/src/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 using ToDoList.Services;
@@ -8,22 +9,31 @@ namespace ToDoList.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _config;
         private readonly ILogger<IndexModel> _logger;
         private readonly ToDoService _todoService;
 
         public int TodoCount { get; private set; }
 
-        public IndexModel(ToDoService todoService, ILogger<IndexModel> logger)
+        public IndexModel(ToDoService todoService, IConfiguration config, ILogger<IndexModel> logger)
         {
             _todoService = todoService;
+            _config = config;
             _logger = logger;
         }
 
         public async Task<IActionResult> OnGet()
         {
             _logger.LogDebug("GET / called");
-            TodoCount = await _todoService.GetToDoCountAsync();
+            
+            if (!string.IsNullOrEmpty(_config["BannerMessage"]))
+            {
+                ViewData["BannerMessage"] = _config["BannerMessage"];
+            }
+
+            TodoCount = await _todoService.GetToDoCountAsync();            
             _logger.LogDebug($"Fetched count: {TodoCount} from service");
+
             return Page();
         }
     }
