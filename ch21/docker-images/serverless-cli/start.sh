4beb42a21c878598a8c3551da1829377cafcfb04
Elton Stoneman <elton@sixeyed.com>
2021-04-09 07:36:13 +0100

    Use FQDN for Kubernetes API server

--- ch21/docker-images/serverless-cli/start.sh
+++ ch21/docker-images/serverless-cli/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # set up access to Kube API
-kubectl config set-cluster default --server=https://kubernetes.default --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+kubectl config set-cluster default --server=https://kubernetes.default.svc.cluster.local --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 kubectl config set-context default --cluster=default
 kubectl config set-credentials user --token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 kubectl config set-context default --user=user
