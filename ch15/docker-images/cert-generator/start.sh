42798de93d3d3c540943d011acc6b58bda7301f2
Elton Stoneman <elton@sixeyed.com>
2021-04-10 08:12:14 +0100

    Only setup Kubectl if we need it

--- ch15/docker-images/cert-generator/start.sh
+++ ch15/docker-images/cert-generator/start.sh
@@ -24,14 +24,14 @@ echo ----------------
 echo Certs generated. 
 echo ----------------
 
-# set up access to Kube API
-kubectl config set-cluster default --server=https://kubernetes.default.svc.cluster.local --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-kubectl config set-context default --cluster=default
-kubectl config set-credentials user --token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-kubectl config set-context default --user=user
-kubectl config use-context default
-
 if [ -n "$CREATE_SECRET" ]; then
+    # set up access to Kube API
+    kubectl config set-cluster default --server=https://kubernetes.default.svc.cluster.local --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+    kubectl config set-context default --cluster=default
+    kubectl config set-credentials user --token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+    kubectl config set-context default --user=user
+    kubectl config use-context default
+
     mv server-cert.pem tls.crt
     mv server-key.pem tls.key
     kubectl create secret tls $CREATE_SECRET --key=tls.key --cert=tls.crt
