c3c2256a07d1b9796ff5d13c299f1916f0653297
Elton Stoneman <elton@sixeyed.com>
2022-08-30 09:16:35 +0100

    Fix multi-arch image tags

--- ch02/docker-images/push-manifests.ps1
+++ ch02/docker-images/push-manifests.ps1
@@ -3,8 +3,8 @@ $images=$(yq e '.services.[].image' docker-compose.yml)
 foreach ($image in $images)
 {    
     docker manifest create --amend $image `
-      "$($image):linux-arm64" `
-      "$($image):linux-amd64"
+      "$($image)-linux-arm64" `
+      "$($image)-linux-amd64"
     
     docker manifest push $image
 }
